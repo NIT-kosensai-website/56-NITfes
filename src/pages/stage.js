3739a6cbe165452441a0d654df3eb6cdcfcec332
Hoppy0616 <hodahoda.20430@gmail.com>
2021-10-25 17:52:37 +0900

    ステージプログラムの表作成

--- src/pages/stage.js
+++ src/pages/stage.js
@@ -3,7 +3,11 @@ import * as React from "react"
 const stagePage = () => {
     return (
         <main>
-
+            <h1>ステージプログラム</h1>
+            <div className="container">
+                <table>
+                </table>
+            </div>
         </main>
     )
 }
