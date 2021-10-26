9f40cdcecdb061cb9741dbf1011cb925998e0c6b
Hoppy0616 <hodahoda.20430@gmail.com>
2021-10-26 20:45:39 +0900

    Linkのパスの修正

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,19 +7,19 @@ const IndexPage = () => {
       <h1 className="text-lg">10/30,10/31 第56回高専祭</h1>
       <ul>
         <li>
-          <Link to="/stagePage">ステージプログラムを見る</Link>
+          <Link to="/stage">ステージプログラムを見る</Link>
         </li>
         <li>
-          <Link to="/mapPage">開場マップを見る</Link>
+          <Link to="/map">開場マップを見る</Link>
         </li>
         <li>
-          <Link to="/stagePage">ステージ企画</Link>
+          <Link to="/stage">ステージ企画</Link>
         </li>
         <li>
-          <Link to="/exhibitionsPage">展示一覧を見る</Link>
+          <Link to="/exhibition">展示一覧を見る</Link>
         </li>
         <li>
-          <Link to="/bOnePage">B1グランプリ</Link>
+          <Link to="/b1">B1グランプリ</Link>
         </li>
       </ul>
     </main>
