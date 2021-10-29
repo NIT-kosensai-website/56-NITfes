1943574caba76e28ad433951444b5091f0468832
Hoppy0616 <hodahoda.20430@gmail.com>
2021-10-30 02:15:40 +0900

    JSONを使わないのでgatsby-configからのjson-formaterの依存関係を削除

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,6 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
-     `gatsby-transformer-json`,
-    {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          name: `data`,
-          path: `${__dirname}/src/data/`,
-        },
-      },
     //tailwindcss関連のプラグイン
     'gatsby-plugin-postcss',
     {
