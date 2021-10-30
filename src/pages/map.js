4a4c2c6b8f24abb216e23874415a71a93fda6f56
4-19-1-10 <okachan2003@outlook.jp>
2021-10-30 14:00:17 +0900

    importの追加

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 const mapPage = () => {
   return (
     <div className="h-screen bg-gray-200">
