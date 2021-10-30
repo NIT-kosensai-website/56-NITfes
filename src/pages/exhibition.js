c209c0a12f7d0c95532a9de61eca2a11eb228652
4-19-1-10 <okachan2003@outlook.jp>
2021-10-30 11:46:39 +0900

    記事準備中...の削除

--- src/pages/exhibition.js
+++ src/pages/exhibition.js
@@ -121,9 +121,6 @@ const exhibitionPage = () => {
           </div>
         </div>
       </div>
-      <div className="flex justify-center py-8">
-        <p className="text-lg">記事準備中...</p>
-      </div>
       <div className="flex justify-center">
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/6 my-8">
           <Link to="/">トップへ戻る</Link>
