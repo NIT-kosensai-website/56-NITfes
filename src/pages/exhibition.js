a2c5bc1014cb6c29c36ceffd069113e3056c6345
4-19-1-10 <okachan2003@outlook.jp>
2021-10-30 10:47:38 +0900

    centerへ

--- src/pages/exhibition.js
+++ src/pages/exhibition.js
@@ -6,7 +6,7 @@ const exhibitionPage = () => {
       <h1 className="text-center text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl pt-8">
         展示
       </h1>
-      <div className="border-solid border-2 border-green-500 max-w-sm overflow-hidden shadow-lg">
+      <div className="flex justify-center rounded border-solid border-2 border-green-500 max-w-sm overflow-hidden shadow-lg">
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">留学生の部屋</div>
           <p className="text-gray-700 text-base">
@@ -15,7 +15,7 @@ const exhibitionPage = () => {
             「留学生の部屋」では留学生が自分たちの出身国の紹介をします。
             ぜひお越しください。
           </p>
-          <div className="px-6 pt-4 pb-2 bg-green-500">
+          <div className="px-6 pt-4 pb-2 bg-green-500 rounded-full">
             <p className="text-white text-base">場所：専攻科棟 102</p>
           </div>
         </div>
