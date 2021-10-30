6423da191263ffb9155c8a94a906380620ef7c28
Hoppy0616 <hodahoda.20430@gmail.com>
2021-10-30 09:26:28 +0900

    ボタンの幅調整

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,26 +7,26 @@ const IndexPage = () => {
       <h1 className="text-center text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl pt-8">
         第56回沼津高専高専祭
       </h1>
-      <ul className="text-center mt-8">
+      <ul className="text-center mt-8 grid grid-cols-1 gap-16 md:gap-6">
         <li className="mt-8">
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/6">
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/6 sm:w-9/12">
             <Link to="/stage">ステージプログラムを見る</Link>
           </button>
         </li>
 
         <li className="mt-8">
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/6">
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/6 sm:w-9/12">
             <Link to="/map">会場マップを見る</Link>
           </button>
         </li>
 
         <li className="mt-8">
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/6">
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/6 sm:w-9/12">
             <Link to="/exhibition">展示一覧を見る</Link>
           </button>
         </li>
         <li className="mt-8">
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/6">
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/6 sm:w-9/12">
             <Link to="/b1">B1グランプリ</Link>
           </button>
         </li>
