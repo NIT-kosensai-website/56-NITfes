ac18f68c2e65ff96552205ef690598bbe6c47d64
4-19-1-10 <okachan2003@outlook.jp>
2021-10-30 11:09:28 +0900

    茶道同好会のカードの追加

--- src/pages/exhibition.js
+++ src/pages/exhibition.js
@@ -65,6 +65,25 @@ const exhibitionPage = () => {
           </div>
         </div>
       </div>
+      <div className="flex justify-center">
+        <div className="rounded border-solid border-2 border-green-500 max-w-sm overflow-hidden shadow-lg">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">茶道同好会</div>
+            <p className="text-base">
+              <p>
+                毎週寮食堂の小部屋で活動しています、茶道同好会です。今年のお茶会ではコロナかということで昨年に引き続き、例年通りの生菓子ではなくお干菓子をお茶請けとしてお出しいたしまｓ。お茶碗ではなく紙コップでの提供となり残念でありますが、ぜひお抹茶を味わってみてください。一席(お茶1杯とお菓子1つ)200円となります。
+              </p>
+              <p>
+                また、畳の上で風炉での薄茶点前の実演も行います。部員一同、練習を重ねてきました。ぜひ見に来てみてください。
+              </p>
+            </p>
+            <div className="px-6 pt-4 pb-2">
+              <p className="text-white text-base bg-green-500 rounded-full">
+                場所：屋外出店
+              </p>
+            </div>
+          </div>
+        </div>
       <div className="flex justify-center py-8">
         <p className="text-lg">記事準備中...</p>
       </div>
