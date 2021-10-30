8337bb4ef79089baf726595eee189bc6a54cecf4
Mr-Ojii <okachan2003@outlook.jp>
2021-10-30 10:41:13 +0900

    留学生の部屋のカードの追加

--- src/pages/exhibition.js
+++ src/pages/exhibition.js
@@ -1,17 +1,31 @@
-import * as React from "react"
+import * as React from "react";
 import { Link } from "gatsby";
 const exhibitionPage = () => {
-    return (
-        <div className="h-screen bg-gray-200">
-        <div className="flex justify-center py-8">
-          <p className="text-lg">少女画像準備中...</p>
-        </div>
-        <div className="flex justify-center">
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/6 my-8">
-            <Link to="/">トップへ戻る</Link>
-          </button>
+  return (
+    <div className="h-screen bg-gray-200">
+      <h1 className="text-center text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl pt-8">
+        展示
+      </h1>
+      <div className="border-solid border-2 border-green-500 max-w-sm overflow-hidden shadow-lg">
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">留学生の部屋</div>
+          <p className="text-gray-700 text-base">
+            沼津高専には、現在、アジアの４ヵ国から来た6６の外国人留学生がいます。
+            ふだん校内や学生寮で姿を見かけることがあっても、留学生たちの母国での暮らしなどはあまり知られていないのではないでしょうか。
+            「留学生の部屋」では留学生が自分たちの出身国の紹介をします。
+            ぜひお越しください。
+          </p>
+          <div className="px-6 pt-4 pb-2 bg-gray-200">
+            <p className="text-base">場所：専攻科棟 102</p>
+          </div>
         </div>
       </div>
-    )
-}
-export default exhibitionPage
+      <div className="flex justify-center">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/6 my-8">
+          <Link to="/">トップへ戻る</Link>
+        </button>
+      </div>
+    </div>
+  );
+};
+export default exhibitionPage;
