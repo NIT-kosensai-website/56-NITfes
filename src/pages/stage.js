4075a802968be44839099b60e1e357ea92f14ccc
4-19-1-10 <okachan2003@outlook.jp>
2021-10-30 15:52:26 +0900

    大文字にする

--- src/pages/stage.js
+++ src/pages/stage.js
@@ -14,7 +14,7 @@ const StagePage = () => {
               <StaticQuery
                 query={graphql`
                   query StageDayOneQuery {
-                    allstagedayoneJson {
+                    allStagedayoneJson {
                       edges {
                         node {
                           head
